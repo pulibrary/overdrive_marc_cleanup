5ce193fcffb23f6c1699d76e6c6234aa98ed25c7
Sean Warren <seanwarren77@gmail.com>
2025-04-08 08:40:01 -0400

    Update ruocop.yml to use class name in tests

--- spec/overdrive_cleaner_spec.rb
+++ spec/overdrive_cleaner_spec.rb
@@ -4,7 +4,7 @@ require 'overdrive_cleaner'
 
 RSpec.describe OverdriveCleaner do
   it 'can be instantiated' do
-    oc = described_class.new
-    expect(oc).to be_instance_of(described_class)
+    oc = OverdriveCleaner.new
+    expect(oc).to be_instance_of(OverdriveCleaner)
   end
 end
